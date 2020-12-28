09b7b35fcf73f58128566a76ee0b747409ed5e93
AlinaAndriychuk <alinka.andriychuk2002@gmail.com>
2020-12-28 14:58:13 +0200

    add files

---
+++ public/build/precache-manifest.2536ba0b0a47df7bfc3e4df83bb53c1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c92c7b4153b370699d27c3a944aa3db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9f647b3ab5bb98a42dc",
+    "url": "/static/css/main.5181a43a.chunk.css"
+  },
+  {
+    "revision": "606c43c4014d353b7550",
+    "url": "/static/js/2.f3c43537.chunk.js"
+  },
+  {
+    "revision": "3377ffda74687e7745ec38a7bdbe5cdb",
+    "url": "/static/js/2.f3c43537.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9f647b3ab5bb98a42dc",
+    "url": "/static/js/main.1d201b40.chunk.js"
+  },
+  {
+    "revision": "797afd30eb378f627c61",
+    "url": "/static/js/runtime-main.d7d96907.js"
+  }
+]);
