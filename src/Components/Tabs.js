d0fa1ba938276dee4aa70fcea33156c9d39be2b8
alina <alina@coderiver.com.ua>
2021-01-20 18:28:12 +0200

    no slider

--- src/Components/Tabs.js
+++ src/Components/Tabs.js
@@ -5,51 +5,24 @@ function TabItem(props) {
   const itemClass = classNames({"tabs__item": true, active: props.active});
 
   return (
-    <li className={itemClass} onClick={ () => props.switchTab(props.options) }>
+    <li className={itemClass} onClick={ () => props.onChange(props.options) }>
       {props.renderTab(props.options.label)}
     </li>
   )
 }
 
 function Tabs(props) {
-  const slider = useRef(null);
-  const sliderPage = useRef(null);
-
-  function switchTab(value) {
-    const pageWidth = sliderPage.current.clientWidth; 
-    slider.current.style.right = pageWidth * (value.value - 1) + "px";
-  
-    props.onChange(value)
-  }
-  
   return (
     <div className="tabs">
       <ul className="tabs__container">
         {
           props.options.map( item => {
             return (
-              <TabItem options={item} switchTab={switchTab} active={props.value.value === item.value} renderTab={props.renderTab} key={item.value}></TabItem>  
+              <TabItem options={item} onChange={props.onChange} active={props.value.value === item.value} renderTab={props.renderTab} key={item.value}></TabItem>  
             )
           })
         }
       </ul>
-      <div className="slider" ref={slider}>
-        <div className="slider__page slider__page--green" ref={sliderPage}>
-          <h1 className="slider__title">
-            Page 1
-          </h1>
-        </div>
-        <div className="slider__page slider__page--red">
-          <h1 className="slider__title">
-            Page 2
-          </h1>
-        </div>
-        <div className="slider__page slider__page--yellow">
-          <h1 className="slider__title">
-            Page 3
-          </h1>
-        </div>
-      </div>
     </div>
   )
 };
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import Tabs from './Components/Tabs';
 
 const options = [{ value: '1', label: 'Tab One'}, { value: '2', label: 'Tab Two'}, { value: '3', label: 'Tab Three'}];
+const compareLabel = ['Tab One', 'Tab two', 'Tab three'];
 
 function View() {
   const [value, setValue] = useState({ value: '1', label: 'Tab One'});
@@ -19,8 +20,37 @@ function View() {
   };
 
   return (
-    <Tabs options={options} value={value} renderTab={renderTab} onChange={handleOnChange}>
-    </Tabs>
+    <>
+      <Tabs options={options} value={value} renderTab={renderTab} onChange={handleOnChange}>
+      </Tabs>
+      {
+        value.label === 'Tab One' && (
+          <div className="page page_green">
+              <h1 className="page__title">
+                Page 1
+              </h1>
+          </div>
+        )
+      }
+      {
+        value.label === 'Tab Two' && (
+          <div className="page page_red">
+              <h1 className="page__title">
+                Page 3
+              </h1>
+          </div>
+        )
+      }
+      {
+        value.label === 'Tab Three' && (
+          <div className="page page_yellow">
+              <h1 className="page__title">
+                Page 2
+              </h1>
+          </div>
+        )
+      }
+    </>
   )
 }
 
